f1a98c767d93137c93e5b7405b4dffef182c947d
Wilson Ahanmisi <50172975+Wilsona4@users.noreply.github.com>
2021-05-30 17:36:13 +0100

    Set up navigation controller

--- app/src/main/java/com/decagonhq/clads/SplashScreenActivity.kt
+++ app/src/main/java/com/decagonhq/clads/SplashScreenActivity.kt
@@ -1,11 +1,24 @@
 package com.decagonhq.clads
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        /*Move to the home page after showing splash screen for X milliseconds*/
+        Handler().postDelayed(
+            {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                /*Use finish to disable the page when back button is pressed from homePage*/
+                finish()
+            },
+            1000
+        )
     }
 }
