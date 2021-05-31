79549328d5a99f0700ae118f2a5c9d0c8a6db7f7
Wilson Ahanmisi <50172975+Wilsona4@users.noreply.github.com>
2021-05-31 03:43:28 +0100

    Feature email signup ui implementation

--- app/src/main/java/com/decagonhq/clads/ui/view/EmailSignUpFragment.kt
+++ app/src/main/java/com/decagonhq/clads/ui/view/EmailSignUpFragment.kt
@@ -4,58 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.decagonhq.clads.R
+import com.decagonhq.clads.databinding.EmailSignUpFragmentBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [EmailSignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EmailSignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: EmailSignUpFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.email_sign_up_fragment, container, false)
+        _binding = EmailSignUpFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EmailSignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EmailSignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        /*Set up Account Category Dropdown*/
+        val accountCategories = resources.getStringArray(R.array.account_category)
+        val accountCategoriesArrayAdapter = ArrayAdapter(requireContext(), R.layout.account_category_dropdown_item, accountCategories)
+        binding.emailSignUpFragmentAccountCategoryTextView.setAdapter(accountCategoriesArrayAdapter)
     }
 }
