5c3e8452d1c799946f54e166986cda844bedb6e3
Unoka Ruth <33810711+Ruthy4@users.noreply.github.com>
2021-06-03 11:02:24 +0100

    Fix the button to navigate from the landing screen

--- app/src/main/java/com/decagonhq/clads/ui/view/LandingScreenFragment.kt
+++ app/src/main/java/com/decagonhq/clads/ui/view/LandingScreenFragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.decagonhq.clads.R
 import com.decagonhq.clads.databinding.FragmentLandingScreenBinding
 
 class LandingScreenFragment : Fragment() {
@@ -22,13 +20,6 @@ class LandingScreenFragment : Fragment() {
         /*Inflate the layout for this fragment*/
         _binding = FragmentLandingScreenBinding.inflate(inflater, container, false)
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.landingButton.setOnClickListener {
-            findNavController().navigate(R.id.action_landingScreenFragment_to_signUpOptionsFragment)
-        }
     }
 
     override fun onDestroyView() {
