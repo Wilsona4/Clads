1e98f01fe070f8ba7443a475d47c03a236c29cfd
Oluwafemijohn <femiogundipe01@gmail.com>
2021-06-03 15:08:17 +0100

    Clean up boilerplate

--- app/src/main/java/com/decagonhq/clads/ui/view/ResetPasswordFragment.kt
+++ app/src/main/java/com/decagonhq/clads/ui/view/ResetPasswordFragment.kt
@@ -7,16 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.decagonhq.clads.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ResetPasswordFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ResetPasswordFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -25,8 +16,7 @@ class ResetPasswordFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -39,23 +29,4 @@ class ResetPasswordFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_reset_password, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ResetPasswordFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ResetPasswordFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
