a2808692c872b4633610df190aa15e06f662b287
devnonny <chinonso.okafor@decagon.dev>
2021-06-04 17:16:43 +0100

    Fix resolve git conflicts again

--- app/src/test/java/com/decagonhq/clads/util/ValidationObjectTest.kt
+++ app/src/test/java/com/decagonhq/clads/util/ValidationObjectTest.kt
@@ -1,18 +1,10 @@
 package com.decagonhq.clads.util
 
-<<<<<<< HEAD
-import com.decagonhq.clads.util.ValidationObject.validateEmail
-import com.decagonhq.clads.util.ValidationObject.validatePasswordMismatch
-import junit.framework.TestCase
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
-=======
 import com.decagonhq.clads.util.ValidationObject.validateAccountCategory
 import com.decagonhq.clads.util.ValidationObject.validateEmail
 import com.decagonhq.clads.util.ValidationObject.validatePasswordMismatch
 import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
->>>>>>> eff8365b10d7154826f515049d49d0537db31269
 import org.junit.Test
 
 class ValidationObjectTest {
@@ -25,31 +17,21 @@ class ValidationObjectTest {
     private val confirmPasswordTrue: String = "Myfa4ourite@Pass"
     private val confirmPasswordFalse: String = "Myfa4our@Pass"
 
-<<<<<<< HEAD
-=======
     private val accountCategory: String = "Tailor"
 
->>>>>>> eff8365b10d7154826f515049d49d0537db31269
     /*Test Validate Email Function with Correct Mail*/
     @Test
     fun test_validateEmail_isTrue() {
         val result = validateEmail(emailTrue)
-<<<<<<< HEAD
-        TestCase.assertTrue(result)
-=======
         assertTrue(result)
->>>>>>> eff8365b10d7154826f515049d49d0537db31269
+
     }
 
     /*Test Validate Email Function with Wrong Email*/
     @Test
     fun test_validateEmail_isFalse() {
         val result = validateEmail(emailFalse)
-<<<<<<< HEAD
-        TestCase.assertFalse(result)
-=======
         assertFalse(result)
->>>>>>> eff8365b10d7154826f515049d49d0537db31269
     }
 
     /*Test Validate Password Mismatch Function with Correct Passwords*/
@@ -58,18 +40,13 @@ class ValidationObjectTest {
         val result = validatePasswordMismatch(password, confirmPasswordTrue)
         assertTrue(result)
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> eff8365b10d7154826f515049d49d0537db31269
     /*Test Validate Password Mismatch Function with wrong Passwords*/
     @Test
     fun test_passwordMismatch_isFalse() {
         val result = validatePasswordMismatch(password, confirmPasswordFalse)
         assertFalse(result)
     }
-<<<<<<< HEAD
-=======
 
     /*Test Validate Account Type Function*/
     @Test
@@ -77,5 +54,4 @@ class ValidationObjectTest {
         val result = validateAccountCategory(accountCategory)
         assertTrue(result)
     }
->>>>>>> eff8365b10d7154826f515049d49d0537db31269
 }
