d95f632956be7b51a5e3fdc5c1bf6022743b38d6
Oluwafemijohn <femiogundipe01@gmail.com>
2021-06-20 00:13:01 +0100

    Unused import removed

--- app/src/main/java/com/decagonhq/clads/ui/profile/editprofile/AccountFragment.kt
+++ app/src/main/java/com/decagonhq/clads/ui/profile/editprofile/AccountFragment.kt
@@ -2,21 +2,18 @@ package com.decagonhq.clads.ui.profile.editprofile
 
 import android.Manifest
 import android.app.Activity
-import android.content.ContentResolver
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.provider.OpenableColumns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContentResolverCompat.query
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
