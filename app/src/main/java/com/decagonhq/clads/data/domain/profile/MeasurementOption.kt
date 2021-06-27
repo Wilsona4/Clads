c89ea35ce59a07bf7247da43b51cd904eb48ebb7
Oluwafemijohn <femiogundipe01@gmail.com>
2021-06-27 19:18:13 +0100

    Updated user profile data class

---
+++ app/src/main/java/com/decagonhq/clads/data/domain/profile/MeasurementOption.kt
@@ -0,0 +1,6 @@
+package com.decagonhq.clads.data.domain.profile
+
+data class MeasurementOption(
+    val acceptSelfMeasurement: Boolean,
+    val visitForMeasurement: Boolean
+)
---
+++ app/src/main/java/com/decagonhq/clads/data/domain/profile/profile.kt
@@ -0,0 +1,7 @@
+package com.decagonhq.clads.data.domain.profile
+
+data class profile(
+    val message: String,
+    val payload: UserProfile,
+    val status: Int
+)
