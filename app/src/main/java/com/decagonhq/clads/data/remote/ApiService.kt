eca16e83e45d371781cdaf2088e95eae11a4e39a
Unoka Ruth <33810711+Ruthy4@users.noreply.github.com>
2021-07-11 06:31:48 +0100

    add upload gallery api service

--- app/src/main/java/com/decagonhq/clads/data/remote/ApiService.kt
+++ app/src/main/java/com/decagonhq/clads/data/remote/ApiService.kt
@@ -8,6 +8,7 @@ import com.decagonhq.clads.data.remote.login.LoginCredentialsDTO
 import com.decagonhq.clads.data.remote.profile.UserProfileDTO
 import com.decagonhq.clads.data.remote.registration.UserRegistrationDTO
 import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Multipart
@@ -37,6 +38,9 @@ interface ApiService {
     @Multipart
     @POST("upload")
     suspend fun uploadImage(@Part image: MultipartBody.Part): GenericResponseClass<UserProfileImage>
+
+    @POST("upload")
+    suspend fun uploadGallery(@Body requestBody: RequestBody): GenericResponseClass<UserProfileImage>
 
     @GET("download")
     fun getUploadedImage(): GenericResponseClass<UserProfileImage>
