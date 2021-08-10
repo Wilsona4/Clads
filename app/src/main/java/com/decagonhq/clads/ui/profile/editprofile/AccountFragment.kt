39e5b7fb49905253ebb5f480f7ac9772a824a27d
Unoka Ruth <33810711+Ruthy4@users.noreply.github.com>
2021-08-10 12:32:35 +0100

    Fix ktlint errors

--- app/src/main/java/com/decagonhq/clads/ui/profile/editprofile/AccountFragment.kt
+++ app/src/main/java/com/decagonhq/clads/ui/profile/editprofile/AccountFragment.kt
@@ -131,14 +131,11 @@ class AccountFragment : BaseFragment() {
                             accountFragmentStateValueTextView.text =
                                 userProfile.workshopAddress?.state ?: getString(R.string.lagos)
                             accountFragmentWorkshopAddressCityValueTextView.text =
-                                userProfile.workshopAddress?.city
-                                ?: getString(R.string.lagos)
+                                userProfile.workshopAddress?.city ?: getString(R.string.lagos)
                             accountFragmentWorkshopAddressStreetValueTextView.text =
-                                userProfile.workshopAddress?.street
-                                ?: getString(R.string.enter_address)
+                                userProfile.workshopAddress?.street ?: getString(R.string.enter_address)
                             accountFragmentShowroomAddressValueTextView.text =
-                                userProfile.showroomAddress?.state
-                                ?: getString(R.string.enter_address)
+                                userProfile.showroomAddress?.state ?: getString(R.string.enter_address)
                             accountFragmentNameOfUnionValueTextView.text = userProfile.union?.name
                                 ?: getString(R.string.enter_union_name)
                             accountFragmentWardValueTextView.text = userProfile.union?.ward
